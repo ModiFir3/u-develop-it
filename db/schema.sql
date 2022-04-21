8bfe93c0be1e0dd1bf46525a0bdbc72e3a23d3c8
PaulS <psoliz333@gmail.com>
2022-04-21 16:22:17 -0500

    joined parties_id to candiates table

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+/* important that we drop the files in the correct order in this case since candidates uses parties id you have to drop candidates first*/
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
+/* important that you set up the tables when using data from one and going into another to put the one on top that you are using data from  i.e. parties to candidates*/
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
@@ -8,5 +13,7 @@ CREATE TABLE candidates(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
+    party_id INTEGER,
+    industry_connected BOOLEAN NOT NULL,
+    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
